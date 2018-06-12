c1600e6442f459866c235b7c33afae962a60e5ba
victor77dev <victor77.dev@gmail.com>
2018-06-12 09:23:33 +0200

    Copy Helmet, Header and Footer from example

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,18 +12,39 @@
  */
 
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+
+const AppWrapper = styled.div`
+  max-width: calc(768px + 16px * 2);
+  margin: 0 auto;
+  display: flex;
+  min-height: 100%;
+  padding: 0 16px;
+  flex-direction: column;
+`;
 
 export default function App() {
   return (
-    <div>
+    <AppWrapper>
+      <Helmet
+        titleTemplate="%s - Guess 2/3"
+        defaultTitle="Guess 2/3"
+      >
+        <meta name="description" content="Guess 2/3 - Guess 2/3 of average" />
+      </Helmet>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
-    </div>
+      <Footer />
+    </AppWrapper>
   );
 }
