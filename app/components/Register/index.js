2910991ca9bf482409bec108cf6d8a99ad264157
victor77dev <victor77.dev@gmail.com>
2018-12-08 20:53:51 +0100

    Update Register Form UI to Material-UI

--- app/components/Register/index.js
+++ app/components/Register/index.js
@@ -7,64 +7,128 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // import styled from 'styled-components';
-import { Field } from 'redux-form/immutable';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
 
-const RenderField = ({ input, label, type, meta: { touched, error, asyncValidating } }) => (
-  <div className="form-group">
-    <label htmlFor={label}>{label}</label>
-    <input {...input} placeholder={label} className="form-control" type={type} />
-    {asyncValidating && <div>Checking availability<div className="loader"></div></div>}
-    {touched && error && <span>{error}</span>}
+import { Field } from 'redux-form/immutable';
+import InlineAlert from 'components/InlineAlert';
+
+const styles = (theme) => ({
+  container: {
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    display: 'block',
+    width: '60%',
+  },
+  textCen: {
+    textAlign: 'center',
+    color: grey['500'],
+    display: 'block',
+    margin: theme.spacing.unit,
+    width: '100%',
+  },
+  textLef: {
+    textAlign: 'left',
+    color: grey['700'],
+    margin: theme.spacing.unit,
+    width: '100%',
+  },
+  buttonDiv: {
+    display: 'flex',
+  },
+  button: {
+    margin: theme.spacing.unit,
+    marginRight: 'auto',
+    marginLeft: 'auto',
+  },
+  textFieldDiv: {
+  },
+  textField: {
+    margin: theme.spacing.unit,
+    width: '100%',
+  },
+  alert: {
+  },
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
+});
+
+const RenderField = ({ input, label, type, meta: { touched, error, asyncValidating }, classes }) => (
+  <div>
+    <Typography variant="headline" component="h4" className={classes.textLef}>{label}</Typography>
+    <div className={classes.textFieldDiv}>
+      <TextField
+        {...input}
+        placeholder={label}
+        type={type}
+        className={classes.textField}
+        InputProps={{
+          endAdornment: <InputAdornment position="end">
+            {asyncValidating && <CircularProgress size={24} className={classes.progress} />}
+          </InputAdornment>,
+        }}
+      />
+      {touched && error && <InlineAlert message={error} variant="error" className={classes.alert} />}
+    </div>
   </div>
 );
+
+const StyledRenderField = withStyles(styles)(RenderField);
 
 RenderField.propTypes = {
   input: PropTypes.object,
   label: PropTypes.string,
   type: PropTypes.string,
   meta: PropTypes.object,
+  classes: PropTypes.object,
 };
 
 class Register extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
-    const { error, handleSubmit, submitting, registerSubmit } = this.props;
+    const { classes, error, handleSubmit, submitting, registerSubmit } = this.props;
     return (
-      <div className="container">
-        <h2 className="page-header">Register</h2>
-        <p>Please register below</p>
-        {error && <div className="alert alert-danger">{error}</div>}
+      <Grid container justify="center" className={classes.container}>
+        <Typography variant="display1" component="h3" className={classes.textCen}>Register Here!</Typography>
+        {error && <InlineAlert message={error} variant="error" />}
         <form onSubmit={handleSubmit(registerSubmit)}>
           <Field
             name="email"
             type="email"
-            component={RenderField}
+            component={StyledRenderField}
             label="Email"
           />
           <Field
             name="username"
             type="text"
-            component={RenderField}
+            component={StyledRenderField}
             label="Username"
           />
           <Field
             name="password1"
             type="password"
-            component={RenderField}
+            component={StyledRenderField}
             label="Password"
           />
           <Field
             name="password2"
             type="password"
-            component={RenderField}
+            component={StyledRenderField}
             label="Confirm Password"
           />
-          <div>
-            <button className="btn btn-primary" type="submit" disabled={submitting}>
+          <div className={classes.buttonDiv}>
+            <Button variant="contained" className={classes.button} color="primary" type="submit" disabled={submitting}>
               Register
-            </button>
+            </Button>
           </div>
         </form>
-      </div>
+      </Grid>
     );
   }
 }
@@ -74,6 +138,7 @@ Register.propTypes = {
   handleSubmit: PropTypes.func,
   submitting: PropTypes.bool,
   registerSubmit: PropTypes.func,
+  classes: PropTypes.object,
 };
 
-export default Register;
+export default withStyles(styles)(Register);
