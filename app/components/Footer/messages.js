530e3c478c2884cecfee3de33ef9a0ac42f81f7d
victor77dev <victor77.dev@gmail.com>
2018-12-11 03:32:10 +0100

    Update Footer messages

--- app/components/Footer/messages.js
+++ app/components/Footer/messages.js
@@ -6,14 +6,10 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  licenseMessage: {
-    id: 'boilerplate.components.Footer.license.message',
-    defaultMessage: 'This project is licensed under the MIT license.',
-  },
   authorMessage: {
     id: 'boilerplate.components.Footer.author.message',
     defaultMessage: `
-      Made by {author}.
+      Developed by {author}.
     `,
   },
 });
